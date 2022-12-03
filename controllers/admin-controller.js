bf003f985da68693d3e203a6b4010640a9cd104f
忍者哈特利 <ks0dcongra@gmail.com>
2022-12-03 17:50:41 +0800

    feat: add admin index page

---
+++ controllers/admin-controller.js
@@ -0,0 +1,7 @@
+const adminController = {
+  getfakeClass: (req, res) => {
+    return res.render('admin/fake-class')
+  }
+}
+
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-
 const router = express.Router()
-
-// 載入controller
+// 載入 controller
 const fakeclassController = require('../controllers/fake-class-controller')
+// 載入 modules
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/fake-class', fakeclassController.getfakeClass)
-
 router.use('/', (req, res) => res.render('fake-class'))
 
 module.exports = router
