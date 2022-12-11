4b2b53710a3c5ac6fd80d4e3d2be7d93dcc820b7
忍者哈特利 <ks0dcongra@gmail.com>
2022-12-11 15:19:30 +0800

    fix: eslint

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router()
 const fakeclassController = require('../controllers/fake-class-controller')
 // 載入 modules
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller') //新增Bcrypt.js
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage) //新增Bcrypt.js
+router.post('/signup', userController.signUp) //新增Bcrypt.js
 router.get('/fake-class', fakeclassController.getfakeClass)
-router.use('/', (req, res) => res.render('fake-class'))
+router.get('/', (req, res) => res.render('fake-class'))
 
 module.exports = router
+
+var appe = "123";
