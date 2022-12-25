3c356a0653d756c28f7c79d57f32a50c2b8ad84a
忍者哈特利 <ks0dcongra@gmail.com>
2022-12-25 16:30:36 +0800

    style: app.js conform to eslint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const methodOverride = require('method-override')
 const app = express()
 const routes = require('./routes')
 const PORT = process.env.PORT || 3000
-const db = require('./models')
+// const db = require('./models')
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -12,7 +12,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({ extended: true })) //body-parser解析controller的body方法
+app.use(express.urlencoded({ extended: true })) // body-parser解析controller的body方法
 app.use(methodOverride('_method'))
 app.use(routes)
 
