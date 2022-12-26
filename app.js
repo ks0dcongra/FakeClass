0d934737a94232734d61552eb118ccd8e1ab8087
忍者哈特利 <ks0dcongra@gmail.com>
2022-12-26 14:59:18 +0800

    feat: flash msg && signup verification

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const methodOverride = require('method-override')
 const app = express()
 const routes = require('./routes')
 const PORT = process.env.PORT || 3000
-// const db = require('./models')
+
+// 新增以下兩行，引入套件flash套件
+const flash = require('connect-flash')
+const session = require('express-session')
+const SESSION_SECRET = 'secret' // 新增這行
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -14,6 +18,15 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true })) // body-parser解析controller的body方法
 app.use(methodOverride('_method'))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  console.log(res)
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
+
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const admin = require('./modules/admin')
 // 載入 controller
 const fakeclassController = require('../controllers/fake-class-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler') // flash-message讓錯誤資訊傳遞下去
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/fake-class', fakeclassController.getfakeClass)
 router.use('/', (req, res) => res.render('fake-class'))
+router.use('/', generalErrorHandler) // flash-message讓錯誤資訊傳遞下去
 
 module.exports = router
