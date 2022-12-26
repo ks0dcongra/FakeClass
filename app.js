19774ad0a2a1670f6793049e89e17fef737900e1
忍者哈特利 <ks0dcongra@gmail.com>
2022-12-26 15:59:30 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+const handlebars = require('express-handlebars')
 const methodOverride = require('method-override')
 const app = express()
 const routes = require('./routes')
 const PORT = process.env.PORT || 3000
 const passport = require('./config/passport') // 增加這行，引入 Passport
-
+const { getUser } = require('./helpers/auth-helpers')
 // 新增以下兩行，引入套件flash套件
 const flash = require('connect-flash')
 const session = require('express-session')
 const SESSION_SECRET = 'secret' // 新增這行
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
@@ -24,9 +25,9 @@ app.use(passport.initialize()) // 增加這行，初始化 Passport
 app.use(passport.session()) // 增加這行，啟動 session 功能
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
-  console.log(res)
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 
